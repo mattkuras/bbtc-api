2dba988dc78a6f190e2778dd22ca9eba838d55ca
matt_kuras <mattkuras@gmail.com>
2021-04-19 14:04:50 -0400

    add admin in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create({
+    username: 'mattkuras',
+    email: 'mattkuras@gmail.com',
+    password: '123456'
+})
